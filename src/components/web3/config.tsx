2948baf709e30870c55c40f9bdd4c710b8383a42
Melissa Privitera <melissa.privitera@protonmail.com>
2024-08-14 13:36:16 +0200

    added walletconnect

---
+++ src/components/web3/config.tsx
@@ -0,0 +1,28 @@
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config"
+
+import { cookieStorage, createStorage } from "wagmi"
+import { shibariumTestnet, baseSepolia, hardhat } from "wagmi/chains"
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if (!projectId) throw new Error("Project ID is not defined")
+
+const metadata = {
+  name: "SheLeads",
+  description: "SheLeads",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+}
+
+const chains = [hardhat, shibariumTestnet, baseSepolia] as const
+
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: false,
+  auth: { email: false },
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+
+import Web3ModalProvider from "@/components/web3/context/appContext"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Web3ModalProvider>
+      <Component {...pageProps} />
+    </Web3ModalProvider>
+  )
 }
