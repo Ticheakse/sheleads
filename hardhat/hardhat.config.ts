4fa6a1c820737bbc187e3866bc0a18af1015d88d
Melissa Privitera <melissa.privitera@protonmail.com>
2024-08-15 20:59:03 +0200

    added shibarium and baseSepolia

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -9,21 +9,48 @@ const config: HardhatUserConfig = {
   defaultNetwork: "localhost",
   networks: {
     shibarium: {
+      url: "https://www.shibrpc.com", // Replace with Shibarium's RPC URL
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    puppynet: {
       url: "https://puppynet.shibrpc.com",
       accounts: [process.env.PRIVATE_KEY as string],
+    },
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 1000000000,
     },
   },
   etherscan: {
     apiKey: {
       shibarium: process.env.ETHERSCAN_API_KEY as string,
+      puppynet: process.env.ETHERSCAN_API_KEY as string,
+      baseSepolia: process.env.BLOCKSCOUT_KEY as string,
     },
     customChains: [
       {
         network: "shibarium",
+        chainId: 109,
+        urls: {
+          apiURL: "https://www.shibariumscan.io/api/",
+          browserURL: "https://www.shibariumscan.io/",
+        },
+      },
+      {
+        network: "puppynet",
         chainId: 157,
         urls: {
           apiURL: "https://puppyscan.shib.io/api/",
           browserURL: "https://puppyscan.shib.io",
+        },
+      },
+      {
+        network: "baseSepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://base-sepolia.blockscout.com/api",
+          browserURL: "https://base-sepolia.blockscout.com",
         },
       },
     ],
