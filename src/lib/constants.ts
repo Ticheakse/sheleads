7be99816435c69e531af7c118757e0441668b858
Melissa Privitera <melissa.privitera@protonmail.com>
2024-08-19 02:20:18 +0200

    updated new contract

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ import { shibarium, shibariumTestnet, baseSepolia, hardhat } from "wagmi/chains"
 
 export const CONTRACT_ADDRESSES: { [chainId: number]: string } = {
   [hardhat.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  [baseSepolia.id]: "0xBE7Fe774DfaDDd92aC71562A0113D85508139AB5",
+  [baseSepolia.id]: "0x9FC47a0A4431470bC6804785F96ae1ABd5B3950E",
   [shibariumTestnet.id]: "",
   [shibarium.id]: "",
 }
