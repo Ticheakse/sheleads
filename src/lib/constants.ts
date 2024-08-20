a33cf465d32fa15c70d49b517a193648db18f1ff
Melissa Privitera <melissa.privitera@protonmail.com>
2024-08-20 20:13:04 +0200

    added shibarium contract

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ import { shibarium, baseSepolia, hardhat } from "wagmi/chains"
 export const CONTRACT_ADDRESSES: { [chainId: number]: string } = {
   [hardhat.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
   [baseSepolia.id]: "0x7485ba960Cf0FEbf9fA0C699300d37c5c3B72799",
-  [shibarium.id]: "",
+  [shibarium.id]: "0x9fF04b2d1be38783D0CAb05205C3caD3D0D71208",
 }
 
 export const algorithm = "aes-256-cbc"
